044b957543d57329d079d4a52b47a17e8591cdb4
lolasegui <lsegui19939@iesjoanramis.org>
2024-10-15 12:53:55 +0200

    Add files via upload

---
+++ ADDTOCART/js/script.js
@@ -0,0 +1,7 @@
+const input= document.getElementyById("inputField");
+const boto= document.getElementyById("afegir");
+
+botto.addventListener ("click", function(){
+    console.log(input.value)
+
+})
